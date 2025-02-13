fabbb02940bda780c65b7af16d08d8589732836a
NephiAppel <appelnephi@gmail.com>
2025-02-13 10:11:42 -0500

    800x800

--- finalCoordinates/src/Components/Grid/grid.tsx
+++ finalCoordinates/src/Components/Grid/grid.tsx
@@ -34,7 +34,7 @@ function Grid() {
 
   
   const gridMaker = () => {
-    for(let i=1; i<=360000; i++){
+    for(let i=1; i<=640000; i++){
       let type=Math.floor(Math.random()*4);
       let gridClass = typeOfGrid[type].name;
       if (playerSet && i == start){
@@ -48,7 +48,7 @@ function Grid() {
       else{
         grids.push(<div className={`${gridClass} grid`} id ={`x${x} y${y}`} key={i}></div>)
       }
-      if (y == 600){
+      if (y == 800){
         y = 0;
         x++;
       }
